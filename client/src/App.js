e0e695698b4477a9e752c5af0bc8ba95395a8888
Melih <melh_akkas@hotmail.com>
2023-05-20 15:18:47 +0300

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,31 @@
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
+
+import Navbar from "./components/Navbar";
+
+import Signin from "./pages/Auth/Signin";
+import Signup from "./pages/Auth/Signup";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Melih</h1>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <div id="content">
+          <Routes>
+            <Route path='/' exact element={<Home />} />
+            <Route path='/Signin' element={<Signin />} />
+            <Route path='/Signup' element={<Signup />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+function Home() {
+  return <h2>Home</h2>;
+}
---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,36 @@
+
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./styles.module.css"
+
+import { Button, ButtonGroup } from '@chakra-ui/react'
+
+function Navbar() {
+    return (
+        <nav className={styles.nav}>
+
+            <div className={styles.left}>
+                <div className="log">
+                    <Link to="/" >e.commerce</Link>
+                </div>
+                <ul className={styles.menu}>
+                    <li>
+                        <Link to="/">Products</Link>
+                    </li>
+                </ul>
+            </div>
+            <div className={styles.right}>
+                <Link to="/Signin" >
+                    <Button colorScheme='blue'>Login</Button>
+                </Link>
+                <Link to="/Signup" >
+                    <Button colorScheme='pink'>Register</Button>
+                </Link>
+
+
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
